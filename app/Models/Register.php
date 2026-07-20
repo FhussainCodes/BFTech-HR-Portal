e6378457d68064e7c521b5da92d4bb7d3b0f2cb4
FhussainCodes <betterpart2300@gmail.com>
2026-07-20 13:59:39 +0500

    make fillables

--- app/Models/Register.php
+++ app/Models/Register.php
@@ -6,7 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Register extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'last_name',
+        'email',
+        'age', 
+        'phone_number', 
+        'password', 
+        'confrim_password', 
+        'city', 
+        'country', 
+        'designation'
+    ];
 
-    
+
 }
