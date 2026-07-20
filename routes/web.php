dc5fbc2c9ab284a73329654f5ce5add1b4e4439d
FhussainCodes <betterpart2300@gmail.com>
2026-07-21 01:06:02 +0500

    make login controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 Route::get("/register",[RegisterController::class,'create']);
 Route::post("/register",[RegisterController::class,'store'])->name('registerUser')->middleware('registercheck');
 
+Route::get('/login',[LoginController::class,'create']);
