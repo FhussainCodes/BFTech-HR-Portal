e6d5715355b29c138d87906b9a75798b22d2f247
FhussainCodes <betterpart2300@gmail.com>
2026-07-22 17:54:34 +0500

    change footer page styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="bg-light text-white py-3 " >
-    <div class="container text-center text-dark" >
-        <p class="mb-0" >
+<footer class="bg-white border-top py-2">
+    <div class="container text-center">
+        <p class="mb-0 text-muted small" style="font-size: 0.75rem;">
             © {{ date('Y') }} BFTech HR Portal. All Rights Reserved.
         </p>
     </div>
