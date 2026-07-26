19801e4015489cbd50b047666cc013540a7d10ca
FhussainCodes <betterpart2300@gmail.com>
2026-07-27 00:55:34 +0500

    write more code

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ForgotPasswordRequest;
 use App\Models\Register;
+use App\Models\PasswordResetOtp;
 
 class ForgotPasswordController extends Controller
 {
@@ -15,7 +16,7 @@ class ForgotPasswordController extends Controller
 
     public function sendOtp(ForgotPasswordRequest $request){
 
-        $email = $request0->email;
+        $email = $request->email;
         $user = Register::where('email',$email)->first();
         if(!$user){
             return back()->withErrors([
@@ -24,6 +25,26 @@ class ForgotPasswordController extends Controller
         }
 
         $otp = random_int(100000,999999);
+        $passwordReset = PasswordResetOtp::where('email',$email)->first();
+
+        if(!$passwordReset){
+            PasswordResetOtp::create([
+                'email' => $email,
+                'otp' => $otp,
+                'expires_at' => now()->addMinutes(5),
+            ]);
+        }else{
+            // $passwordReset->otp = $otp;
+            // $passwordReset->expires_at = now()->addMinutes(5);
+            // $passwordReset->save();
+            $PasswordResetOtp::update([
+                    'otp' => $otp,
+                    'expires_at' => now()->addMinutes(5),
+            ]);
+        }
+
+        Mail::to($email)->queue(new OptMail($otp));
+
 
     }
 }
