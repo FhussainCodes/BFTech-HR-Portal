666e43175e13ddbfae891a45c60c2ffa732d6d16
FhussainCodes <betterpart2300@gmail.com>
2026-07-27 18:02:50 +0500

    make rules and mesages in leave request file

--- app/Http/Requests/LeaveRequest.php
+++ app/Http/Requests/LeaveRequest.php
@@ -12,7 +12,7 @@ class LeaveRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,28 @@ class LeaveRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'leave_type' => 'required',
+            'from_date' => 'required|date|after_or_equal:today',
+            'to_date' => 'required|date|after_or_equal:from_date',
+            'reason' => 'nullable',
         ];
     }
+
+    public function messages(): array
+{
+    return [
+
+        'leave_type.required' => 'Please select a leave type.',
+
+        'from_date.required' => 'Please select the leave start date.',
+        'from_date.date' => 'Please enter a valid start date.',
+        'from_date.after_or_equal' => 'Leave cannot be applied for a past date.',
+
+        'to_date.required' => 'Please select the leave end date.',
+        'to_date.date' => 'Please enter a valid end date.',
+        'to_date.after_or_equal' => 'The end date must be the same as or after the start date.',
+
+        'reason.string' => 'Reason must be valid text.',
+    ];
+}
 }
