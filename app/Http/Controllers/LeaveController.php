849dd180ca3326d7995a9a11044701e5287512d0
FhussainCodes <betterpart2300@gmail.com>
2026-08-09 00:00:09 +0500

    use this in leave controller

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\LeaveRequest;
 use App\Models\Leave;
+use App\Models\Register;
+use App\Notifications\LeaveAppliedNotification;
 class LeaveController extends Controller
 {
     public function index(){
@@ -26,6 +28,11 @@ class LeaveController extends Controller
         $validatedData['employee_id'] = session('user')['id'];
 
         $leaves = Leave::create($validatedData);
+        $hrUsers = Register::where('role', 'hr')->get();
+
+        foreach ($hrUsers as $hr) {
+            $hr->notify(new LeaveAppliedNotification($leaves));
+        }
 
         return redirect()->route('leave.index.show')->with('success', 'Leave applied successfully.');
     }
