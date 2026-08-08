8b2ff9016bf0348f175c189f8e658f9fa33b0335
FhussainCodes <betterpart2300@gmail.com>
2026-08-08 23:53:40 +0500

    make notification class

---
+++ app/Notifications/LeaveAppliedNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Leave;
+
+class LeaveAppliedNotification extends Notification
+{
+    use Queueable;
+    public $leave;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Leave $leave)
+    {
+        $this->leave = $leave;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        // return ['mail'];
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'leave_id' => $this->leave->id,
+
+            'employee_name' =>
+                $this->leave->employee->first_name . ' ' .
+                $this->leave->employee->last_name,
+
+            'leave_type' => $this->leave->leave_type,
+
+            'from_date' => $this->leave->from_date,
+
+            'to_date' => $this->leave->to_date,
+
+            'message' => 'New leave request has been submitted.',
+        ];
+    }
+
+    
+}
