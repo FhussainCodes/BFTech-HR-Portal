faa2d11ec454a5228d53ae61cba17c7ada5a9548
FhussainCodes <betterpart2300@gmail.com>
2026-08-25 16:25:41 +0500

    make public blade file and also add public url in env file

---
+++ resources/views/hr/profile/public.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>
+        {{ $user->first_name }} {{ $user->last_name }}
+    </title>
+
+    <meta property="og:title"
+          content="{{ $user->first_name }} {{ $user->last_name }}">
+
+    <meta property="og:description"
+          content="{{ $user->role }}">
+
+    @if($user->profile_image)
+
+        <meta property="og:image"
+              content="https://fragrance-legroom-bannister.ngrok-free.dev/storage/{{ $user->profile_image }}">
+
+    @endif
+
+    <meta property="og:url"
+          content="https://fragrance-legroom-bannister.ngrok-free.dev/profile/{{ $user->id }}">
+
+    <meta property="og:type"
+          content="profile">
+
+</head>
+
+<body>
+
+    <h1>
+        {{ $user->first_name }}
+        {{ $user->last_name }}
+    </h1>
+
+    @if($user->profile_image)
+
+        <img src="https://fragrance-legroom-bannister.ngrok-free.dev/storage/{{ $user->profile_image }}"
+             width="300">
+
+    @endif
+
+    <p>
+        {{ $user->role }}
+    </p>
+
+</body>
+</html>
