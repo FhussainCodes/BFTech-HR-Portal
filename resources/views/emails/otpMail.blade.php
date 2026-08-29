109ee1f1efd0ab509d22a18d65595873b8b94bd4
FhussainCodes <betterpart2300@gmail.com>
2026-08-29 22:01:19 +0500

    update blade files of emails folder

--- resources/views/emails/otpMail.blade.php
+++ resources/views/emails/otpMail.blade.php
@@ -1,41 +1,19 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>Password Reset OTP</title>
 </head>
-
 <body>
-
     <h2>BFTech HR Portal</h2>
-
     <p>Hello,</p>
-
-    <p>
-        We received a request to reset your password.
-    </p>
-
-    <p>
-        Your One-Time Password (OTP) is:
-    </p>
-
+    <p>We received a request to reset your password.</p>
+    <p>Your One-Time Password (OTP) is:</p>
     <h1>{{ $otp }}</h1>
-
-    <p>
-        This OTP will expire in <strong>5 minutes</strong>.
-    </p>
-
-    <p>
-        If you did not request a password reset, please ignore this email.
-    </p>
-
+    <p>This OTP will expire in <strong>5 minutes</strong>.</p>
+    <p>If you did not request a password reset, please ignore this email.</p>
     <br>
-
     <p>Regards,</p>
-
     <strong>BFTech HR Portal</strong>
-
 </body>
-
 </html>
--- resources/views/emails/register-alert.blade.php
+++ resources/views/emails/register-alert.blade.php
@@ -1,37 +1,33 @@
 @extends('layouts.email')
+
 @section('content')
+<h2>Welcome to BFTech HR Portal</h2>
 
-     <h2>Welcome to BFTech HR Portal</h2>
+<p>Hello {{ $user->first_name }} {{ $user->last_name }},</p>
 
-    <p>Hello {{ $user->first_name }} {{ $user->last_name }},</p>
+<p>Your account has been created successfully.</p>
 
-    <p>
-        Your account has been created successfully.
-    </p>
-
-    <p>You can now log in to the BFTech HR Portal.</p>
-    <p>
+<p>You can now log in to the BFTech HR Portal.</p>
+<p>
     <a href="{{ route('loginPage') }}">Login to BFTech HR Portal</a>
-    </p>
+</p>
 
-    <h3>Account Details</h3>
+<h3>Account Details</h3>
 
-    <p><strong>Name:</strong> {{ $user->first_name }} {{ $user->last_name }}</p>
+<p><strong>Name:</strong> {{ $user->first_name }} {{ $user->last_name }}</p>
 
-    <p><strong>Email:</strong> {{ $user->email }}</p>
+<p><strong>Email:</strong> {{ $user->email }}</p>
 
-    <p><strong>Designation:</strong> {{ $user->designation }}</p>
+<p><strong>Designation:</strong> {{ $user->designation }}</p>
 
-    <p><strong>City:</strong> {{ $user->city }}</p>
+<p><strong>City:</strong> {{ $user->city }}</p>
 
-    <p>
-        If you did not create this account, please contact the HR Administrator.
-    </p>
+<p>If you did not create this account, please contact the HR Administrator.</p>
 
-    <br>
+<br>
 
-    <p>Thank you.</p>
+<p>Thank you.</p>
 
-    <p>BFTech HR Portal Team</p>
-    
+<p>BFTech HR Portal Team</p>
+
 @endsection
